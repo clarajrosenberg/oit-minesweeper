f9056b4ed31dcebd654f3999aa8c6af9a14b1ff0
clarajrosenberg <33033258+clarajrosenberg@users.noreply.github.com>
2022-11-28 20:07:55 -0500

    remove command line minesweeper

--- minesweeper.py
+++ minesweeper.py
@@ -92,45 +92,6 @@ class Board:
         
         pygame.display.update()
 
-    def __str__(self):
-        visible_board = [[None for _ in range(self.dim_size)] for _ in range(self.dim_size)]
-        for row in range(self.dim_size):
-            for col in range(self.dim_size):
-                if (row, col) in self.dug:
-                    visible_board[row][col] = str(self.board[row][col])
-                else:
-                    visible_board[row][col] = " "
-        
-        string_rep = ""
-        widths = []
-        for index in range(self.dim_size):
-            columns = map(lambda x: x[index], visible_board)
-            widths.append(len(max(columns, key = len)))
-        
-        indices = [i for i in range(self.dim_size)]
-        indices_row = "   "
-        cells = []
-        for index, col in enumerate(indices):
-            format = "%-" + str(widths[index]) + "s"
-            cells.append(format % (col))
-        indices_row += "  ".join(cells)
-        indices_row += "  \n"
-
-        for i in range(len(visible_board)):
-            row = visible_board[i]
-            string_rep += f"{i} |"
-            cells = []
-            for index, col in enumerate(row):
-                format = "%-" + str(widths[index]) + "s"
-                cells.append(format % (col))
-            string_rep += " |".join(cells)
-            string_rep += " |\n"
-        
-        str_len = int(len(string_rep) / self.dim_size)
-        string_rep = indices_row + "-"*str_len + "\n" + string_rep + "-"*str_len
-
-        return string_rep
-
 
 def play(dim_size = 10, num_bombs = 10):
     pygame.init()
